d965fb3b135330387fdff3c18de168840893204a
Musharraf188 <mr.musharraf188@gmail.com>
2023-08-03 15:31:12 +0530

    Card/Applied

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,15 +1,20 @@
+import Card from '../UI/Card';
+import classes from './AddUser.module.css';
+
 const AddUser =()=>{
     const formHandler =(event)=>{
         event.preventDefault();
     }
     return (
+        <Card className={classes.input}>
       <form onSubmit={formHandler}>
         <label htmlFor="user">UserName</label>
-        <input id="user" type="text"/>
+        <input  id="user" type="text"/>
         <label htmlFor="age">Age (in Years)</label>
         <input id="age" type="text"/>
         <button type="submit">Add User</button>
       </form>
+      </Card>
     )
 }
 
