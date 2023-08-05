9e839367d0b2bbc0d53c6761db0795fc9a288521
Musharraf188 <mr.musharraf188@gmail.com>
2023-08-05 13:00:35 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import React from 'react';
-import AddUser from './components/Users/AddUser';
-import UserList from './components/Users/UserList';
 
 
 function App() {
-  const receivedUserData =(userData)=>{
-    
-  }
   return (
     <div>
-<AddUser onSave={receivedUserData}/>
-<UserList users={[]}/>
+
     </div>
   );
 }
